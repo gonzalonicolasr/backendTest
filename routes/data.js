3641d16baf04182b532e423d823225f157712703
gonzalonicolasr <gonn.nicolas@gmail.com>
2021-08-16 21:51:38 -0300

    add route request

--- routes/data.js
+++ routes/data.js
@@ -2,19 +2,8 @@ var express = require("express");
 var router = express.Router();
 var request = require("request");
 
-router.get("/:tagId", function (req, res, next) {
-  var url = req.params.tagId;
-
-  request(
-    {
-      uri: "https://" + url,
-    },
-    function (error, response, body) {
-      var obj = {};
-      obj = body;
-      res.status(200).send(body);
-    }
-  );
+router.get("/", function (req, res, next) {
+  res.status(200).send("body");
 });
 
 module.exports = router;
