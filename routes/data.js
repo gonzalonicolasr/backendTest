cd0e39cefed40d0fc47ffafc6a62168b081ecca8
gonzalonicolasr <gonn.nicolas@gmail.com>
2021-08-16 21:48:30 -0300

    add route request

--- routes/data.js
+++ routes/data.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function (req, res, next) {
-  res.status(200).send(body);
+  res.status(200).send("body");
 });
 
 module.exports = router;
