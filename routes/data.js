e0da7feb0b23c983e993400b05601b80449bf340
gonzalonicolasr <gonn.nicolas@gmail.com>
2021-08-16 21:42:32 -0300

    add route request

--- routes/data.js
+++ routes/data.js
@@ -1,20 +1,8 @@
 var express = require("express");
 var router = express.Router();
-var request = require("request");
 
-router.get("/*", function (req, res, next) {
-  var url = req.params[0];
-
-  request(
-    {
-      uri: "http://" + url,
-    },
-    function (error, response, body) {
-      var obj = {};
-      obj = body;
-      res.status(200).send(body);
-    }
-  );
+router.get("/", function (req, res, next) {
+  res.status(200).send(body);
 });
 
 module.exports = router;
