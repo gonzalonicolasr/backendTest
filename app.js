502757effaa656089f239fc70118c77317fea4c9
gonzalonicolasr <gonn.nicolas@gmail.com>
2021-08-18 13:25:59 -0300

    add test

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-
+const server = http.createServer(process.env.PORT || 3000);
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/data");
-
+const port = process.env.PORT || 8000;
 var app = express();
 app.use(cors());
 app.use(logger("dev"));
@@ -17,4 +17,11 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/datos", usersRouter);
 
+app.listen(process.env.PORT || 8000, function () {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
+});
 module.exports = app;
