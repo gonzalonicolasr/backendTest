f35958366ac9f7eb96ab3e4a823d0d0d4ba5b037
gonzalonicolasr <gonn.nicolas@gmail.com>
2021-08-18 12:14:48 -0300

    add test

--- routes/data.js
+++ routes/data.js
@@ -1,16 +1,23 @@
 var express = require("express");
-const fetch = require("node-fetch");
+var fetch = require("node-fetch");
 var router = express.Router();
 
 router.get("/", async function (req, res, next) {
-  let response = await getWeather();
-  await res.status(200).send(response);
-});
-async function getWeather() {
-  const weather = await fetch(
-    "https://steamcommunity.com/id/emmik/inventory/json/730/2"
+  let response = await fetch(
+    "https://steamcommunity.com/id/emmik/inventory/json/730/2/",
+    {
+      method: "GET",
+      body: JSON.stringify("Hello World!"),
+      headers: {
+        "Content-Type": "application/json; charset=UTF-8",
+        Host: "https://*.airtableblocks.com",
+        Accept: "application/json",
+      },
+    }
   );
-  let response = await weather.json();
-  return response;
-}
+  await res.status(200).send(await response.text());
+});
+
 module.exports = router;
+
+console.log(await response.text());
